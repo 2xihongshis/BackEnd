0437eab0676645e58f4fd7d5294e5faf630eac9d
YunJia <sanchuanguli@163.com>
2022-08-14 22:32:52 +0800

    修改 Searcher 配置的文件数据读取方式

--- src/main/java/cn/org/twotomatoes/monitor/config/IpSearcherConfig.java
+++ src/main/java/cn/org/twotomatoes/monitor/config/IpSearcherConfig.java
@@ -3,9 +3,10 @@ package cn.org.twotomatoes.monitor.config;
 import org.lionsoul.ip2region.xdb.Searcher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
-import java.util.Objects;
+import java.io.InputStream;
 
 /**
  * @author HeYunjia
@@ -16,11 +17,14 @@ public class IpSearcherConfig {
 
     @Bean
     public Searcher searcher() throws IOException {
-        String dbPath = Objects.requireNonNull(
-                getClass().getClassLoader().getResource("xdb/ip2region.xdb"))
-                .getPath();
-        byte[] cBuff = Searcher.loadContentFromFile(dbPath);
-        return Searcher.newWithBuffer(cBuff);
+        InputStream dbStream = new ClassPathResource("xdb/ip2region.xdb").getInputStream();
+        byte[] data = new byte[dbStream.available()];
+
+        int readCount = 0;
+        while (readCount < data.length)
+            readCount += dbStream.read(data, readCount, data.length - readCount);
+
+        return Searcher.newWithBuffer(data);
     }
 
 }
